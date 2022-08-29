d4dbbf47cd88d3ed2053eafcea5d429c0b11e122
Vasylyi Bevz <bevzvasya@gmail.com>
2022-08-29 13:27:01 +0300

    first commit

---
+++ src/main/java/com/example/springboot/rickandmortyapp/RickAndMortyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.rickandmortyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyAppApplication.class, args);
+    }
+
+}
